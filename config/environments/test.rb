26892af3ad818fd860b677e195db579330aef7c9
asif-meer <asif_meer_2k6@yahoo.com>
2012-05-18 16:24:17 +0500

    Environment setting done for cucumber and rspec to run

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,9 +2,9 @@ ClinicMis::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
+  # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
+  # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
@@ -13,6 +13,10 @@ ClinicMis::Application.configure do
 
   # Log error messages when you accidentally call methods on nil
   config.whiny_nils = true
+
+  # mailer code from devise module 
+  config.action_mailer.default_url_options = { :host => 'localhost:4000' }
+
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -29,9 +33,14 @@ ClinicMis::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # Use SQL instead of Active Record's schema dumper when creating the test database.
+  # This is necessary if your schema can't be completely dumped by the schema dumper,
+  # like if you have constraints or database-specific column types
+  # config.active_record.schema_format = :sql
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
+  config.assets.allow_debugging = true
 end
