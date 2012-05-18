4c934916ff3df8c68183ef9b403fdeda5274bac1
asif-meer <asif_meer_2k6@yahoo.com>
2012-05-18 16:05:15 +0500

    project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClinicMis::Application.config.secret_token = '48ee23260bfd3d5e463c0d0df4a7264ed691fc995e9531675932f3109c5ef701c0684b36bdbbb8252de716727019e57a911a83e42f404208912621f3ea406815'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClinicMis::Application.config.session_store :cookie_store, key: '_clinic_mis_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClinicMis::Application.config.session_store :active_record_store
